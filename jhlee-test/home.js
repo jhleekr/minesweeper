9029cc40cf2e3249be502ff08273a72e5543fa8e
JongHyeon Lee <jhlee@bfy.kr>
2021-04-28 18:27:28 +0900

    update core

--- jhlee-test/home.js
+++ jhlee-test/home.js
@@ -1,9 +1,8 @@
 // JavaScript Document
-function setUrlParams(params) {
-	window.location.href = 'http://www.abc.com/';
-}
-function getUrlParams() {
-    var params = {};
-    window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(str, key, value) { params[key] = value; });
-    return params;
+function generate() {
+	var xt = document.getElementById('xsize').value;
+	var yt = document.getElementById('ysize').value;
+	var x = parseInt(xt);
+	var y = parseInt(yt);
+	setUrlParams({'x':x,'y':y});
 }
