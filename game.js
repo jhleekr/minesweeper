84ac7e04b797c13ad9e659725d57dd617aa680a8
JongHyeon Lee <jhlee@bfy.kr>
2021-04-29 11:57:53 +0900

    add refreshview, edit click handlers and some buttons

--- game.js
+++ game.js
@@ -1,52 +1,113 @@
 // JavaScript Document
 function reset() {
-	setUrlParams({},"home.html");
+  setUrlParams({}, "home.html");
+}
+var f = false;
+
+function onflag_handler() {
+  if (f) {
+    var flagbtn = document.getElementById('flag');
+    flagbtn.setAttribute('value', 'FLAG');
+    f = false;
+  } else {
+    var flagbtn = document.getElementById('flag');
+    flagbtn.setAttribute('value', 'FIND');
+    f = true;
+  }
 }
 var x = 0;
 var y = 0;
-var t = 0;
+
 function init() {
-	var params = getUrlParams();
-	try {
-		x = params['x'] * 1;
-		y = params['y'] * 1;
-	} catch (e) {
-		alert('Error: '+e);
-		reset();
-		return;
-	}
-	if (!(x > 0)) {
-		alert('Error: Invalid X');
-		reset();
-		return;
-	}
-	if (!(y > 0)) {
-		alert('Error: Invalid Y');
-		reset();
-		return;
-	}
-	document.writeln('<table>');
-	for (let a = 0; a < x; a++) {
-		document.writeln('<tr>');
-		for (let b = 0; b < y; b++) {
-			document.writeln('<td>');
-			document.writeln('<input id="x'+(a+1)+'y'+(b+1)+'" class="unknown_mine" type="button" value=" " onclick="ms.onclick_handler(this)">');
-			document.writeln('</td>');
-		}
-		document.writeln('</tr>');
-	}
-	document.writeln('</table>');
-	document.writeln('<input id=reset" class="button_general" type="button" value="RESET" onclick="reset()">');
-}
-var ms = {
-	'map': [],
-	'onclick_handler': function (self) {
-		var xs = parseInt(self.id.split('x')[1].split('y')[0]);
-		var ys = parseInt(self.id.split('y')[1]);
-		if (t === 0) {
-			this.map = setMine(x, y, xs, ys);
-		}
-		t += 1;
-	}
+  var params = getUrlParams();
+  try {
+    x = parseInt(params['x']);
+    y = parseInt(params['y']);
+  } catch (e) {
+    alert('Error: ' + e);
+    reset();
+    return;
+  }
+  if (!(x > 0)) {
+    alert('Error: Invalid X');
+    reset();
+    return;
+  }
+  if (!(y > 0)) {
+    alert('Error: Invalid Y');
+    reset();
+    return;
+  }
+  document.writeln('<table>');
+  for (let a = 0; a < x; a++) {
+    document.writeln('<tr>');
+    for (let b = 0; b < y; b++) {
+      document.writeln('<td>');
+      document.writeln('<input id="x' + (a + 1) + 'y' + (b + 1) + '" class="unknown_mine" type="button" value=" " onclick="ms.onclick_handler(this)">');
+      document.writeln('</td>');
+    }
+    document.writeln('</tr>');
+  }
+  document.writeln('</table>');
+  document.writeln('<input id="reset" class="button_general" type="button" value="RESET" onclick="reset()">');
+  document.writeln('<input id="flag" class="button_general" type="button" value="FLAG" onclick="onflag_handler()">');
 }
 init();
+function gameover() {
+  document.writeln('game over!');
+}
+var ms = {
+  'map': [],
+  'flagmap': [],
+  't': 0,
+  'refreshview': function () {
+    for (let ix = 0; ix < x; ix++) {
+      for (let iy = 0; iy, y; iy++) {
+        var tmpbtn = document.getElementById('x' + (ix + 1) + 'y' + (iy + 1));
+        var mapdata = this.map[ix][iy];
+        var classstr = "";
+        //Process with tmpbtn
+        switch (mapdata) {
+          case -2: //not mine, not opened
+          case -1: //mine, not opened
+            classstr = "unknown_mine";
+            break;
+          case 0:
+          case 1:
+          case 2:
+          case 3:
+          case 4:
+          case 5:
+          case 6:
+          case 7:
+          case 8: //not mine, opened
+            classstr = "not_mine";
+            break;
+          case -3: //mine, opened
+            classstr = "exploded_mine"
+            gameover();
+            break;
+        }
+        if (this.flagmap[ix][iy] === 1 && classstr === "unknown_mine") {
+          classstr = "flagged_mine";
+        }
+        tmpbtn.setAttribute('class', classstr);
+      }
+    }
+  },
+  'onclick_handler': function (self) {
+    var xs = parseInt(self.id.split('x')[1].split('y')[0]);
+    var ys = parseInt(self.id.split('y')[1]);
+    if (this.t === 0) {
+      this.map = setMine(x, y, xs, ys);
+    } else {
+		if (f) {
+			this.flagmap = flagMine(x, y, xs, ys, flagmap);
+		} else {
+      		this.map = processMine(x, y, xs, ys, map);
+		}
+    }
+    this.refreshview();
+    this.t += 1;
+  }
+}
